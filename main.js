39f0593531ae46879813c37c114a4cfa35fd9db3
die-01 <scottforall09@example.com>
2026-06-21 20:17:10 +0700

    Initial commit propre

---
+++ main.js
@@ -0,0 +1,26 @@
+
+const form = document.getElementById('contact-form');
+const textarea = document.getElementById('message');
+const counter = document.getElementById('char-count');
+
+// 1. Gestion du compteur de caractères
+textarea.oninput = function () {
+    const len = this.value.length;
+    counter.textContent = `${len} / 180`;
+
+    if (len >= 180) {
+        counter.className = "absolute top-[-16px] right-2 text-[10px] font-bold text-red-500";
+    } else {
+        counter.className = "absolute top-[-16px] right-2 text-[10px] font-medium text-neutral-400";
+    }
+};
+
+// 2. Soumission silencieuse à Netlify
+form.onsubmit = function (e) {
+    e.preventDefault();
+    fetch('/', { method: 'POST', body: new FormData(this) })
+        .then(() => {
+            this.reset();
+            document.getElementById('msg-succes').classList.remove('hidden');
+        });
+};
